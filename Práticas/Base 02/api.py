97085b2c4ea0dd419594b6876b8daa25f39f5f7a
João Victor <jvcgomes@gmail.com>
2024-02-02 17:21:29 -0300

    middleware, suporte para arquivos estáticos, handlers de exceçãocustomizáveis

--- Práticas/Base 02/api.py
+++ Práticas/Base 02/api.py
@@ -5,12 +5,17 @@ from requests import Session
 from wsgiadapter import WSGIAdapter
 import os
 from jinja2 import Environment, FileSystemLoader
+from whitenoise import WhiteNoise
+from middleware import Middleware
 
 class API:
 
-    def __init__(self, templates_dir="templates"):
+    def __init__(self, templates_dir="templates", static_dir="static"):
         self.routes = {}
         self.templates_env = Environment(loader=FileSystemLoader(os.path.abspath("templates")))
+        self.exception_handler = None
+        self.whitenoise = WhiteNoise(self.wsgi_app, root=static_dir)
+        self.middleware = Middleware(self)
 
     def template(self, template_name, context=None):
         if context is None:
@@ -34,12 +39,19 @@ class API:
 
         return wrapper
 
-    def __call__(self, environment, start_response):
+    def wsgi_app(self, environment, start_response):
         request = Request(environment)
-
         response = self.handle_request(request)
-
         return response(environment, start_response)
+
+    def __call__(self, environment, start_response):
+        path_info = environment["PATH_INFO"]
+
+        if path_info.startswith("/static"):
+            environment["PATH_INFO"] = path_info[len("/static"):]
+            return self.whitenoise(environment, start_response)
+
+        return self.middleware(environment, start_response)
 
     def default_response(self, response):
         response.status_code = 404 
@@ -50,15 +62,23 @@ class API:
 
         handler, kwargs = self.find_handler(request_path=request.path)
 
-        if handler is not None:
-            if inspect.isclass(handler):
-                handler = getattr(handler(), request.method.lower(), None)
-                if handler is None:
-                    raise AttributeError("Method now allowed", request.method)
+        try: 
 
-            handler(request, response, **kwargs)
-        else:
-            self.default_response(response)
+            if handler is not None:
+                if inspect.isclass(handler):
+                    handler = getattr(handler(), request.method.lower(), None)
+                    if handler is None:
+                        raise AttributeError("Method now allowed", request.method)
+
+                handler(request, response, **kwargs)
+            else:
+                self.default_response(response)
+        
+        except Exception as e: 
+            if self.exception_handler is None:
+                raise e 
+            else:
+                self.exception_handler(request, response, e)
 
         return response
 
@@ -74,6 +94,12 @@ class API:
         session = Session()
         session.mount(prefix=base_url, adapter=WSGIAdapter(self))
         return session 
+
+    def add_exception_handler(self, exception_handler):
+        self.exception_handler = exception_handler
+
+    def add_middleware(self, middleware_cls):
+        self.middleware.add(middleware_cls)
 
     
 
--- Práticas/Base 02/app.py
+++ Práticas/Base 02/app.py
@@ -1,4 +1,5 @@
 from api import API
+from middleware import Middleware
 # waitress-serve --listen=*:8000 app:app
 
 app = API()
@@ -8,8 +9,8 @@ def home(request, response):
         response.text = "<h1> Essa é a primeira página <h1/> "
 
 @app.route("/home")
-def home(request, response):
-        response.text = "Hello! This is the HOME page!"
+def exception_throwing_handler(request, response):
+    raise AssertionError("This handler should not be user")
 
 @app.route("/about")
 def about(request, response):
@@ -31,3 +32,16 @@ app.add_route("/alternative", alternative_add_route)
 @app.route("/template_test")
 def template_test_handler(req, resp):
         resp.body = app.template("index.html", context={"name": "Nome da página", "title": "Título da página"}).encode()
+
+def custom_exception(request, response, exception_cls):
+        response.text = "Oops! Something went wrong. Please contact our support."
+app.add_exception_handler(custom_exception)
+
+class SimpleCustomMiddleware(Middleware):
+        def process_request(self, req):
+                print("Processing request ", req.url)
+        
+        def process_response(self, req, resp):
+                print("Processing response ", req.url)
+
+app.add_middleware(SimpleCustomMiddleware)
